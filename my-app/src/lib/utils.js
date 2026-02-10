1cb9d3aa84c419446c77908080b9576830d3090b
MuthuGanesh-dev <muthuganesh3012@gmail.com>
2026-02-10 05:50:43 +0000

    Update project: Testcase 3

--- my-app/src/lib/utils.js
+++
@@ -1,5 +0,0 @@
-export function cn(...inputs) {
-  // Lightweight className combiner. If you later install `clsx` and
-  // `tailwind-merge` you can replace this with a merged implementation.
-  return inputs.flat().filter(Boolean).join(" ");
-}
